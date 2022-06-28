79f3a04e29f0cb2796b815f0a8223a6a2a414e8e
ljn <ljnchn@qq.com>
2022-06-28 16:04:05 +0800

    up

--- routes/admin.php
+++ routes/admin.php
@@ -6,7 +6,7 @@ Route::get('/admin/user/test', [\App\Admin\Controller\Test::class, 'index']);
 Route::post('/admin/user/login', [\App\Admin\Controller\Index::class, 'login']);
 
 Route::group('/admin/user/', function () {
-    Route::get('info', [\App\Admin\Controller\Index::class, 'info']);
+    Route::post('info', [\App\Admin\Controller\Index::class, 'info']);
     Route::post('logout', [\App\Admin\Controller\Index::class, 'logout']);
 })->middleware([
     App\Middleware\Auth::class,
