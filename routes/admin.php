45c1dc816a329aa2dc7236f50529e0f495b3ef1d
ljn <ljnchn@qq.com>
2022-08-06 10:13:48 +0800

    up

--- routes/admin.php
+++ routes/admin.php
@@ -6,7 +6,7 @@ use Webman\Route;
 //Route::get('/admin/captchaImage', [\App\Admin\Controller\Index::class, 'captchaImage']);
 //Route::post('/admin/login', [\App\Admin\Controller\Index::class, 'login']);
 
-Route::group('/admin/', function () {
+Route::group('/api/', function () {
     Route::get('test', [\App\Admin\Controller\Test::class, 'index']);
     Route::get('captchaImage', [\App\Admin\Controller\Index::class, 'captchaImage']);
     Route::post('login', [\App\Admin\Controller\Index::class, 'login']);
@@ -15,7 +15,7 @@ Route::group('/admin/', function () {
     App\Middleware\TraceLog::class,
 ]);
 
-Route::group('/admin/', function () {
+Route::group('/api/', function () {
     Route::get('getInfo', [\App\Admin\Controller\Index::class, 'getInfo']);
     Route::get('getRouters', [\App\Admin\Controller\Index::class, 'getRouters']);
     Route::post('logout', [\App\Admin\Controller\Index::class, 'logout']);
