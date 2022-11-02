2f4bd11062eb48e9153af8d4168808a4fa47ce00
ljn <ljnchn@qq.com>
2022-11-02 16:07:09 +0800

    up

--- app/Middleware/AccessControl.php
+++ app/Middleware/AccessControl.php
@@ -9,16 +9,13 @@ class AccessControl implements MiddlewareInterface
 {
     public function process(Request $request, callable $handler) : Response
     {
-        $header = [
-            'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Allow-Origin' => $request->header('Origin', '*'),
-            'Access-Control-Allow-Methods' => '*',
-            'Access-Control-Allow-Headers' => '*',
-        ];
+//        $header = [
+//            'Access-Control-Allow-Credentials' => 'true',
+//            'Access-Control-Allow-Origin' => $request->header('Origin', '*'),
+//            'Access-Control-Allow-Methods' => '*',
+//            'Access-Control-Allow-Headers' => '*',
+//        ];
 
-        $response = $request->method() == 'OPTIONS' ? response('', 200, $header) : $handler($request);
-        $response->withHeaders($header);
-
-        return $response;
+        return $request->method() == 'OPTIONS' ?  response('') : $handler($request);
     }
 }
