b9a13cc05502616c23ef41a1b48b3c17b49c943a
ljn <ljnchn@qq.com>
2022-11-03 15:22:44 +0800

    up

--- app/Admin/Validate/BaseValidate.php
+++ app/Admin/Validate/BaseValidate.php
@@ -13,16 +13,41 @@ class BaseValidate extends Validate
      */
     protected $batch = false;
 
+    /**
+     * 获取单个错误信息
+     * @return string
+     */
     function getError(): string
     {
         $error = parent::getError();
         return is_array($error) ? $error[0] : $error;
     }
 
+    /**
+     * 获取多个错误信息数组
+     * @return array
+     */
     function getErrors(): array
     {
         $error = parent::getError();
         return is_array($error) ? $error : [$error];
+    }
+
+    /**
+     * 根据场景值过滤输入的数据
+     * @param array  $data
+     * @param string $scene
+     * @return array
+     */
+    function filterData(array $data, string $scene = ''): array
+    {
+        $fields = $this->scene[$scene] ?? array_keys($this->rule);
+        foreach ($data as $k => $v) {
+            if (!in_array($k, $fields)) {
+                unset($k);
+            }
+        }
+        return $data;
     }
 
 }
