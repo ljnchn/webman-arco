2e09d66888d157dbe59af74e6f041913bd4e5211
ljn <ljnchn@qq.com>
2023-02-06 15:55:16 +0800

    up

--- app/Api/Controller/Index.php
+++ app/Api/Controller/Index.php
@@ -2,19 +2,51 @@
 
 namespace App\Api\Controller;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use support\Request;
 use support\Response;
 
 class Index
 {
-    public function index(Request $request): Response
+    public function index(): Response
     {
-        return successJson();
+        $client  = new Client();
+        $url     = 'https://api.openai.com/v1/completions';
+        $headers = [
+            'Content-Type'  => 'application/json',
+            'Authorization' => '',
+        ];
+        $request = new \GuzzleHttp\Psr7\Request('POST', $url, $headers);
+
+        $prompt = request()->get('query');
+        $model  = 'text-davinci-003';
+        if (!$prompt) {
+            return failJson('输入内容错误');
+        }
+
+        $body = [
+            'model' => $model,
+            'prompt' => $prompt,
+            'temperature' => 0,
+            'max_tokens' => 2048
+        ];
+        $return = '';
+        try {
+            $response = $client->send($request, ['body' => json_encode($body), 'stream' => true]);
+            $body     = $response->getBody();
+            while (!$body->eof()) {
+                $return .= $body->read(1024);
+            }
+        } catch (GuzzleException $e) {
+            return failJson($e->getMessage());
+        }
+        return successJson([$return]);
     }
 
     public function user(Request $request, $uid): Response
     {
-        return successJson([],'uid is ' . $uid);
+        return successJson([], 'uid is ' . $uid);
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Webman\Route;
 
+Route::get('/test/text', [\App\Api\Controller\Index::class, 'index']);
 Route::group('/api/user', function () {
     Route::get('login', [\App\Api\Controller\Index::class, 'index']);
     Route::get('logout', [\App\Api\Controller\Index::class, 'user']);
